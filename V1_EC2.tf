f172b7c9ea474292dc0d51b0dccd258c6265e48f
RajaYanamala <rajadevops994@gmail.com>
2023-10-04 19:28:14 +0530

    Modifications

--- V1_EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-
-resource "aws_instance" "TeamProject" {
-    ami = "ami-03d294e37a4820c21"
-    instance_type = "t2.micro"
-    key_name = "Project_one_key"
-}
